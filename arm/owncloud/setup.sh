9000bf64061e7fd6a8f3ede34096b3c05f57cb7e
Leonel Nunez <leonel@enelserver.com>
2016-11-10 12:13:28 -0700

    updated to version 9.1.2

--- arm/owncloud/setup.sh
+++ arm/owncloud/setup.sh
@@ -5,7 +5,7 @@ echo "setupiando ando"
 
 mkdir /var/sotolito/vols/php-apps/
 cd /var/sotolito/vols/php-apps
-tar xvf /owncloud-9.0.1.tar.bz2
+tar xvf /owncloud-9.1.2.tar.bz2
 chown -R nginx:nginx owncloud
 
 
