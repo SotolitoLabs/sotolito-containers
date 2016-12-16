9eebdb81c3b612017ed81a04dfda58885dff1b69
Leonel Nunez <leonel@enelserver.com>
2016-12-15 17:54:39 -0700

    upgraded to 1.2.3

--- arm/roundcube/setup.sh
+++ arm/roundcube/setup.sh
@@ -5,9 +5,9 @@ echo "setupiando ando"
 
 mkdir /var/sotolito/vols/php-apps/
 cd /var/sotolito/vols/php-apps
-tar xvf /roundcubemail-1.2.2-complete.tar.gz
-chown -R nginx:nginx roundcubemail-1.2.2
-ln -s roundcubemail-1.2.2 roundcube
+tar xvf /roundcubemail-1.2.3-complete.tar.gz
+chown -R nginx:nginx roundcubemail-1.2.3
+ln -s roundcubemail-1.2.3 roundcube
 
 
 
