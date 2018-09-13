bb8757f237d19183750f134e851e2bf444a18ee2
Ivan Chavero <ichavero@redhat.com>
2018-09-12 18:45:37 -0600

    Add tmp directory to setup

--- alpine/nginx-setup/setup.sh
+++ alpine/nginx-setup/setup.sh
@@ -5,7 +5,7 @@ echo "setupiando ando"
 
 mkdir /var/sotolito/
 cd /var/sotolito 
-#mkdir -p vols/nginx/var/lib/nginx/tmp
+mkdir -p vols/nginx/var/tmp/nginx
 mkdir -p vols/nginx/var/log/nginx
 tar xvf /volconf.tar.bz2
 chown 100 /var/sotolito/vols/nginx/var/lib/nginx
