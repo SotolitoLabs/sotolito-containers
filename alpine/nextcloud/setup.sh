eef4c8a45920d854d6fb82f991a3173d40038d09
Ivan Chavero <ichavero@chavero.com.mx>
2019-09-29 13:19:29 -0600

    Simplify setup for nexcloud

--- alpine/nextcloud/setup.sh
+++ alpine/nextcloud/setup.sh
@@ -5,7 +5,7 @@ echo "Setting up Nextcloud..."
 
 mkdir /var/sotolito/vols/php-apps/
 cd /var/sotolito/vols/php-apps
-tar xvf /nextcloud-14.0.0.tar.bz2
+tar xvf /nextcloud.tar.bz2
 chown -R nginx:nginx nextcloud
 
 
